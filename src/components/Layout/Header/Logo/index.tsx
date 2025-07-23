6050d36de0b4c14995d7cca9ed6000070d0bced2
seinge-club <seinge_club@hotmail.com>
2025-07-23 01:19:18 -0500

    First commit

---
+++ src/components/Layout/Header/Logo/index.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface LogoProps {
+  width?: number;
+  height?: number;
+  className?: string;
+}
+const Logo: React.FC<LogoProps> = ({ width = 180, height = 70, className }) => {
+  return (
+    <Link href="/" className={className}>
+      <Image
+        src="/images/logo/logo_Seinge.svg"
+        alt="logo"
+        width={width}
+        height={height}
+        style={{ width: "auto", height: "auto" }}
+        quality={100}
+      />
+    </Link>
+  );
+};
+
+export default Logo;
